f3a7431e5d2b991df212d695450cb56135fc949d
MarkusKonk <m.konkol@52north.org>
2023-11-29 16:13:07 +0100

    start implementing demo workflow

---
+++ tools/demo_workflow/filterData.R
@@ -0,0 +1,27 @@
+library("getopt")
+library("dplyr")
+
+options(stringAsfactors = FALSE, useFancyQuotes = FALSE)
+
+args = commandArgs(trailingOnly=TRUE)
+option_specification = matrix(c(
+  'inputData', 'i1', 1, 'character',
+  'checked', 'i2', 1, 'character',
+  'accuracy', 'i3', 1, 'double',
+  'outputData', 'o', 2, 'character'
+), byrow=TRUE, ncol=4);
+options = getopt(option_specification);
+
+temperature_values <- read.csv(file=options$inputData, sep = ';', header = TRUE)
+
+if ( options$checked ) {
+  temperature_values <- dplyr::filter(temperature_values, temperature_values$quality_level == "checked")
+}
+
+cat("\n inputData", options$inputData)
+cat("\n checked: ", options$checked)
+cat("\n accuracy: ", options$accuracy)
+cat("\n outputData: ", options$outputData)
+  
+temperature_values <- dplyr::filter(temperature_values, temperature_values$accuracy < options$accuracy)
+write.csv(temperature_values, options$outputData, quote = FALSE)
