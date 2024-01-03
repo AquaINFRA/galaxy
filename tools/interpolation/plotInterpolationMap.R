8f31023c216d9332f08e2caa49b111ea2cc0de2d
MarkusKonk <m.konkol@52north.org>
2024-01-03 11:41:19 +0100

    add parameters to idw interpolation

--- tools/interpolation/plotInterpolationMap.R
+++ tools/interpolation/plotInterpolationMap.R
@@ -7,33 +7,55 @@ library("gstat")
 
 args = commandArgs(trailingOnly=TRUE)
 option_specification = matrix(c(
-    'dataObservations', 'i1', 1, 'character',
+    'observationsCsv', 'i1', 1, 'character',
     'studyArea', 'i2', 1, 'character',
-    'spatialData', 'i3', 1, 'character',
-    'idwPower', 'i4', 1, 'double',
-    'testCase', 'i5', 1, 'character',
+    'idwPower', 'i3', 1, 'double',
+    'samplePoints', 'i4', 1, 'double',
+    'sampleType', 'i5', 1, 'character',
+    'legendLabel', 'i6', 1, 'character',
+    'legendPosition', 'i7', 1, 'character',
+    'dotSize', 'i9', 1, 'double',
+    'colorType', 'i10', 1, 'character',
+    'testCase', 'i8', 1, 'character',
     'outputData', 'o', 2, 'character'
 ), byrow=TRUE, ncol=4);
 options = getopt(option_specification);
 
-cat("\n test", options$testCase)
-cat("\n data", options$dataObservations)
-cat("\n polygon", options$studyArea)
-cat("\n points", options$spatialData)
+cat("\n observationsCsv", options$observationsCsv)
+cat("\n studyArea", options$studyArea)
 cat("\n idwPower", options$idwPower)
+cat("\n samplePoints", options$samplePoints)
+cat("\n sampleType", options$sampleType)
+cat("\n legendLabel", options$legendLabel)
+cat("\n legendposition", options$legendPosition)
+cat("\n dotSize", options$dotSize)
+cat("\n colorType", options$colorType)
+cat("\n testCase", options$testCase)
 cat("\n outputData: ", options$outputData)
 
-input_data <- read.csv(file=options$dataObservations, sep = ',', header = TRUE)
-polygon <- as_Spatial(st_read(options$studyArea))
-points <- as_Spatial(st_read(options$spatialData))
-power <- options$idwPower
+obsData <- read.csv(file=options$observationsCsv, sep = ',', header = TRUE)
+studyArea <- options$studyArea
+idwPower <- options$idwPower
+samplePoints <- options$samplePoints
+sampleType <- options$sampleType
+legendLabel <- options$legendLabel
+legendPosition <- options$legendPosition
+dotSize <- options$dotSize
+colorType <- options$colorType
+testCase <- options$testCase
 
-runInterpolation <- function(points, values, interPolationPower){
-  if (options$testCase == "true") {
+coordinates(obsData) <- c("longitude", "latitude")
+sf_obsData <- as_Spatial(st_as_sf(obsData))
+
+polygon <- as_Spatial(st_read(studyArea))
+sf_obsData@bbox<-polygon@bbox
+
+runInterpolation <- function(points, values, interpolation_power, sample_points, sample_type){
+  if (testCase == "true") {
     cat("\n set seed!")
     set.seed(123)
   }
-  grd              <- as.data.frame(spsample(points, "regular", n=50000))
+  grd              <- as.data.frame(spsample(points, sample_type, n=sample_points))
   names(grd)       <- c("X", "Y")
   coordinates(grd) <- c("X", "Y")
   gridded(grd)     <- TRUE
@@ -41,24 +63,24 @@ runInterpolation <- function(points, values, interPolationPower){
   
   proj4string(points) <- proj4string(points)
   proj4string(grd) <- proj4string(points)
-  return(gstat::idw(values ~ 1, points, newdata=grd, idp=interPolationPower))
+  return(gstat::idw(values ~ 1, points, newdata=grd, idp=interpolation_power))
 }
 
-plotInterpolationMap <- function(raster, points){
+plotInterpolationMap <- function(raster, points, legend_label){
   plot <- tm_shape(raster) + 
-    tm_raster(n=10,palette = rev(brewer.pal(7, "RdBu")), auto.palette.mapping = FALSE,
-              title="Temperature measurements (C°)") +
-    tm_shape(points) + tm_dots(size=0.2) +
-    tm_legend(legend.outside=TRUE)
+    tm_raster(n=10,palette = rev(brewer.pal(7, colorType)), auto.palette.mapping = FALSE,
+              title=legend_label) +
+    tm_shape(points) + tm_dots(size=dotSize) +
+    tm_legend(legend.outside=legendPosition)
   return(plot)
 }
 
-points.idw <- runInterpolation(points, input_data$measurement, power)
+sf_obsData.idw <- runInterpolation(sf_obsData, obsData$measurement, idwPower, samplePoints, sampleType)
 
-raster_object       <- raster(points.idw)
+raster_object       <- raster(sf_obsData.idw)
 raster_object.mask  <- mask(raster_object, polygon)
 
-idw <- plotInterpolationMap(raster_object.mask, points)
+idw <- plotInterpolationMap(raster_object.mask, sf_obsData, legendLabel)
 idw
 
 png(options$outputData)
