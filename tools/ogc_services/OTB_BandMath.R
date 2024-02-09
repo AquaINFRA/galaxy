86c9b47da0f27d55c897ed7b4d6c5c482d6964fe
MarkusKonk <m.konkol@52north.org>
2024-02-09 12:08:10 +0100

    improve otb band math tool

--- tools/ogc_services/OTB_BandMath.R
+++ tools/ogc_services/OTB_BandMath.R
@@ -2,32 +2,29 @@ library("httr2")
 library("jsonlite")
 library("getopt")
 
-args = commandArgs(trailingOnly=TRUE)
-option_specification = matrix(c(
-    'url', 'i1', 2, 'character',
-    'outputData', 'o', 2, 'character'
-), byrow=TRUE, ncol=4);
-options = getopt(option_specification);
+args <- commandArgs(trailingOnly = TRUE)
+option_specification <- matrix(c(
+  'url', 'i1', 1, 'character',
+  'outputData', 'o', 2, 'character'
+), byrow = TRUE, ncol = 4)
+options <- getopt(option_specification)
 
-# Define the URL
 url <- "https://testbed19.geolabs.fr:8717/ogc-api/processes/OTB.BandMath/execution"
 
-
-# Define the JSON data
 json_data <- list(
-  inputs=list(
+  inputs = list(
     il = list(
       list(
         href = options$url
       )
-    ), 
+    ),
     out = "float",
     exp = "im1b3,im1b2,im1b1",
     ram = 256
   ),
-  outputs=list(
-    out=list(
-      format=list(
+  outputs = list(
+    out = list(
+      format = list(
         mediaType = "image/jpeg"
       ),
       transmissionMode = "reference"
@@ -36,24 +33,30 @@ json_data <- list(
   response = "raw"
 )
 
-# Make the POST request
-resp <- request(url) %>%
-  req_headers(
-    'accept' = '/*',
-    'Prefer' = 'return=representation',
-    'Content-Type' = 'application/json'
-  ) %>%
-  req_body_json(json_data) %>%
-  req_perform()
+tryCatch({
+  resp <- request(url) %>%
+    req_headers(
+      'accept' = '/*',
+      'Prefer' = 'return=representation',
+      'Content-Type' = 'application/json'
+    ) %>%
+    req_body_json(json_data) %>%
+    req_perform()
 
-# Check the response
-if (TRUE) {
-  #res <- content(resp, as = "parsed")
-  # Output the response
-  content <- resp$body
-  
-  # Write the content to a JPEG file
-  writeBin(content, options$outputData)
-} else {
-  print("Error: Request failed.")
-}
+  if (resp$status_code == 200) {
+    if (resp$body) {
+      print("Request successful.")
+      content <- resp$body
+      writeBin(content, options$outputData)
+    } else {
+      print("Error: Request failed. No body received.")
+    }
+  } else {
+    print(paste("HTTP", resp$status_code, "Error:", resp$status_message))
+  }
+}#, 
+  #error = function(e) {
+  #print("An unexpected error occurred.")
+  #print(e)
+#}
+)
