06af98ab7e429914d9eddf4ccff1abdb9d84b7b6
MarkusKonk <m.konkol@52north.org>
2024-04-05 14:59:47 +0200

    add example to use input in command

--- tools/ogc_services/generic_meanshift.R
+++ tools/ogc_services/generic_meanshift.R
@@ -112,3 +112,92 @@ make_response_body_readable <- function(body) {
   return(json_object)
 }
 
+tryCatch({
+  # Request 1
+  resp1 <- request(paste0(base_url, execute)) %>%
+    req_headers(
+      "accept" = "/*",
+      "Prefer" = "respond-async;return=representation",
+      "Content-Type" = "application/json"
+    ) %>%
+    req_body_json(json_data) %>%
+    req_perform()
+  response <- make_response_body_readable(resp1$body)
+  status_code1 <- resp1$status_code
+  if (status_code1 == 201) {
+    status <- "running"
+    attempt <- 1
+    while (status == "running") {
+      # Request 2
+      resp2 <- request(paste0(base_url, get_status, response$jobID)) %>%
+        req_headers(
+          "accept" = "application/json"
+        ) %>%
+        req_perform()
+      status_code2 <- resp2$status_code
+      if (status_code2 == 200) {
+        response2 <- make_response_body_readable(resp2$body)
+        cat("\n", response2$status)
+        if (response2$status == "successful") {
+          status <- "successful"
+          # Request 3
+          resp3 <- request(paste0(
+            base_url,
+            get_status, response2$jobID, get_result
+          )) %>%
+            req_headers(
+              "accept" = "application/json"
+            ) %>%
+            req_perform()
+          status_code3 <- resp3$status_code
+          if (status_code3 == 200) {
+            response3 <- make_response_body_readable(resp3$body)
+            if (output_format == "download") {
+              options(timeout = 600)
+              download.file(response3$fout$href,
+                destfile = paste0("output1.", options$outputType),
+                mode = "wb"
+              )
+              download.file(response3$foutpos$href,
+                destfile = paste0("output2.", options$outputType),
+                mode = "wb"
+              )
+            } else if (output_format == "getUrl") {
+              writeLines(paste(response3$fout$href, response3$foutpos$href,
+                           sep = "\n"
+                         ), con = "output.txt")
+            }
+          } else if (status_code3 == 404) {
+            print("The requested URI was not found.")
+          } else if (status_code3 == 500) {
+            print("A server error occurred.")
+          } else {
+            print(paste("HTTP", status_code3, "Error:", resp3$status_message))
+          }
+        } else if (response2$status == "failed") {
+          status <- "failed"
+          message("An error occurred. For further details, check OGC Job status through https://ospd.geolabs.fr:8300/ogc-api/jobs/", response2$jobID)
+          q(status = 1)
+        }
+      } else {
+        status <- "failed"
+        print(paste("HTTP", status_code2, "Error:", resp2$status_message, "An error occurred. For further details, check OGC Job status through https://ospd.geolabs.fr:8300/ogc-api/jobs/", response2$jobID))
+        q(status = 1)
+      }
+      Sys.sleep(3)
+    }
+    print(status)
+  } else if (status_code1 == 400) {
+    print("A query parameter has an invalid value.")
+  } else if (status_code1 == 404) {
+    print("The requested URI was not found.")
+  } else if (status_code1 == 500) {
+    print("The requested URI was not found.")
+  } else {
+    print(paste("HTTP", status_code1, "Error:", resp1$status_message))
+  }
+}, error = function(e) {
+  message("An error occurred:", e)
+  # Exit with code 1
+  q(status = 1)
+})
